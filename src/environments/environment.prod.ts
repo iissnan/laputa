693c83ac3d23d41cec13ef5689d63c2455e4b46f
iissnan <iissnan@gmail.com>
2018-11-22 22:24:43 +0800

    Deployment Setting Up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  contentful: {
+    space: 'gtkoyktsa6dc',
+    token: '70d25ca34bbde060701f03f095daae9d6ee4a4c4b18fb405154bea22e1b40107',
+    locale: 'zh',
+    contentTypes: {
+      game: 'game',
+      platform: 'platform',
+      genre: 'genre',
+    }
+  },
 };
